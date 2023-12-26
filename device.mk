b6e3f89e35706f00c0a6fa446631417bac02daea
Albert I <kras@raphielgang.org>
2023-12-26 21:46:55 +0530

    sweet: Inherit several Android Go configurations
    * Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: Shikhar <krtonia@gmail.com>
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- device.mk
+++ device.mk
@@ -433,6 +433,8 @@ PRODUCT_COPY_FILES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
