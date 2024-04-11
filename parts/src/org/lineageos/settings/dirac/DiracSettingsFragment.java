sweet-xiaomi/device_xiaomi_sweet

sweet: Update XiaomiParts from lisa device tree
History:

commit be0b6a7c6582de694b583eee4a52c8377af80c1a
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Sun May 15 14:02:06 2022 +0530

    sm6150-common: sepolicy: Allow system_app to write thermal sysfs

     * required for xiaomi parts thermal profiles

    Change-Id: I10c14088b591720801b51e412a8c666ebc53e31e

commit 36f226ecc3497437e00c4edbd5679e9dc76bad25
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Thu Oct 13 23:25:05 2022 +0530

    sm6150-common: parts: Use S theme in thermal settings

    The new spinner theme in T (v33) is too large.
    Ref: https://github.com/AOSPA/android_frameworks_base/commit/da5db6f9db991193aab5bf1cb47f13a8087a5a47

    Change-Id: I6ae59548e1fb417ea681158df844bcd17ef8bf56

commit b717f60b9abf701b1e30c3541ee2a169431448be
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Thu Oct 13 12:06:35 2022 +0530

    sm6150-common: parts: Restore MiSound scene on boot complete

    This seems to be necessary only for scene (and not
    other parameters).

    Change-Id: I8bc3f2bd280174a3628dc2458ff969d66c1e79b3

commit 2c378805e55571fa85742e9ba3ae8a06c23eef12
Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
Date:   Sat Jul 23 01:29:10 2022 +0530

    sm6150-common: parts: Open settings on QS tile long press

    Since we have only one QS tile, being MiSound, open
    its settings activity on long presing the tile, instead
    of the default behavior of opening app info.

    Change-Id: Ie3b3444a975a471c2d3ee83eac9347d028521c18

commit 98116844a386fa8828ea8d38eb587734c8b35eb1
Author: Lostark13 <tushar9915@gmail.com>
Date:   Sat Mar 19 21:00:00 2022 +0300

    sm6150-common: parts: Update MiSound headset types

    From lisa V13.0.4.0.SKOMIXM

    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I59e24d429b948dcf5a57449e4f58fc8db58d7627

commit 9899e5be5aaee7053d8aec545e4788e79711b2cd
Author: Lostark13 <tushar9915@gmail.com>
Date:

parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2018,2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.dirac;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Switch;
+
+import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
+import org.lineageos.settings.R;
+
+public class DiracSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener, OnMainSwitchChangeListener {
+
+    private static final String PREF_ENABLE = "dirac_enable";
+    private static final String PREF_HEADSET = "dirac_headset_pref";
+    private static final String PREF_PRESET = "dirac_preset_pref";
+
+    public static final String PREF_SCENE = "scenario_selection";
+
+    private MainSwitchPreference mSwitchBar;
+
+    private ListPreference mHeadsetType;
+    private ListPreference mPreset;
+    private ListPreference mScenes;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dirac_settings);
+
+        DiracUtils.initialize(getActivity());
+        boolean enhancerEnabled = DiracUtils.isDiracEnabled();
+
+        mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(enhancerEnabled);
+
+        mHeadsetType = (ListPreference) findPreference(PREF_HEADSET);
+        mHeadsetType.setOnPreferenceChangeListener(this);
+        mHeadsetType.setEnabled(enhancerEnabled);
+
+        mPreset = (ListPreference) findPreference(PREF_PRESET);
+        mPreset.setOnPreferenceChangeListener(this);
+        mPreset.setEnabled(enhancerEnabled);
+
+        mScenes = (ListPreference) findPreference(PREF_SCENE);
+        mScenes.setOnPreferenceChangeListener(this);
+        mScenes.setEnabled(enhancerEnabled);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case PREF_HEADSET:
+                DiracUtils.setHeadsetType(Integer.parseInt(newValue.toString()));
+                return true;
+            case PREF_PRESET:
+                DiracUtils.setLevel((String) newValue);
+                return true;
+            case PREF_SCENE:
+                DiracUtils.setScenario(Integer.parseInt(newValue.toString()));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        mSwitchBar.setChecked(isChecked);
+
+        DiracUtils.setMusic(isChecked);
+        mHeadsetType.setEnabled(isChecked);
+        mPreset.setEnabled(isChecked);
+        mScenes.setEnabled(isChecked);
+    }
+}

romiyusnandar/android_device_xiaomi_sweet

parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -1,0 +1,103 @@
+/*
+ * Copyright (C) 2018,2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.dirac;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+import org.lineageos.settings.R;
+
+public class DiracSettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener, OnCheckedChangeListener {
+
+    private static final String PREF_ENABLE = "dirac_enable";
+    private static final String PREF_HEADSET = "dirac_headset_pref";
+    private static final String PREF_PRESET = "dirac_preset_pref";
+
+    public static final String PREF_SCENE = "scenario_selection";
+
+    private MainSwitchPreference mSwitchBar;
+
+    private ListPreference mHeadsetType;
+    private ListPreference mPreset;
+    private ListPreference mScenes;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dirac_settings);
+
+        DiracUtils.initialize(getActivity());
+        boolean enhancerEnabled = DiracUtils.isDiracEnabled();
+
+        mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(enhancerEnabled);
+
+        mHeadsetType = (ListPreference) findPreference(PREF_HEADSET);
+        mHeadsetType.setOnPreferenceChangeListener(this);
+        mHeadsetType.setEnabled(enhancerEnabled);
+
+        mPreset = (ListPreference) findPreference(PREF_PRESET);
+        mPreset.setOnPreferenceChangeListener(this);
+        mPreset.setEnabled(enhancerEnabled);
+
+        mScenes = (ListPreference) findPreference(PREF_SCENE);
+        mScenes.setOnPreferenceChangeListener(this);
+        mScenes.setEnabled(enhancerEnabled);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case PREF_HEADSET:
+                DiracUtils.setHeadsetType(Integer.parseInt(newValue.toString()));
+                return true;
+            case PREF_PRESET:
+                DiracUtils.setLevel((String) newValue);
+                return true;
+            case PREF_SCENE:
+                DiracUtils.setScenario(Integer.parseInt(newValue.toString()));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        mSwitchBar.setChecked(isChecked);
+
+        DiracUtils.setMusic(isChecked);
+        mHeadsetType.setEnabled(isChecked);
+        mPreset.setEnabled(isChecked);
+        mScenes.setEnabled(isChecked);
+    }
+}