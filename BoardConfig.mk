81998936483629b03c02c07fa1bf70c2af547d72
AdarshGrewal <adarshgrewal@gmail.com>
2024-04-12 13:57:07 +0530

    sweet: include MIUICamera if exists
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,3 +47,6 @@ VENDOR_SECURITY_PATCH := 2023-11-01
 
 # Inherit from proprietary files
 include vendor/xiaomi/sweet/BoardConfigVendor.mk
+
+# Inherit from proprietary files for miuicamera
+-include vendor/xiaomi/sweet-miuicamera/products/board.mk
