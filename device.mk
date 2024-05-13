ca39fefd1a8c59a99e921b5f61727096b081d041
Pranav Vashi <neobuddy89@gmail.com>
2024-05-14 00:54:05 +0530

    sweet: Add haptic, thermal and refresh rate settings
    Squashed.
    Not adding to common tree since not all devices have all refresh rate modes.
    
    Change-Id: I8a81c34f9469cc593a6c7019996150794d0ddee5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: donjohanliebert <donjohanliebert@gmail.com>
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     DSPVolumeSynchronizer \
     XiaomiDolby \
-    XiaomiDolbyResCommon
+    XiaomiDolbyResCommon \
+    XiaomiParts
 
 # Display
 PRODUCT_COPY_FILES += \
