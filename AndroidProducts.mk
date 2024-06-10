95d587605f5fa30cc2c9bcea58cdc7721ef57b23
omnitrix21 <okand7946@gmail.com>
2024-06-10 23:14:37 +0530

    sweet: An enchanted sweets
    Signed-off-by: omnitrix21 <okand7946@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/halcyon_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    halcyon_sweet-user \
+    halcyon_sweet-userdebug \
+    halcyon_sweet-eng
--- halcyon_sweet.mk
+++ halcyon_sweet.mk
@@ -11,14 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := halcyon_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
+
+TARGET_SCREEN_WIDTH := 1080p
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
