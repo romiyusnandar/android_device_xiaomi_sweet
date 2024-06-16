f0c7795a7ee98434df6484a045c98140b4196afd
mrizallr <yusromi04@gmail.com>
2024-06-16 14:24:14 +0700

    Bringup Orion-OS

--- orion_sweet.mk
+++ orion_sweet.mk
@@ -11,10 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Orion stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+ORION_MAINTAINER := sirNewbies
+ORION_MAINTAINER_LINK := https://t.me/romiyusna
+ORION_GAPPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := orion_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
