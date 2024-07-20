c02a34173346e0b3fd228366afa45eafd7ca0af9
romiyusnandar <yusromi04@gmail.com>
2024-07-20 21:58:33 +0700

    sweet: Bringup ```OrionOS```

--- orion_sweet.mk
+++ orion_sweet.mk
@@ -11,20 +11,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Halcyon stuff.
-$(call inherit-product, vendor/halcyon/config/common.mk)
+# Inherit some common Orion stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := halcyon_sweet
+PRODUCT_NAME := orion_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-
-TARGET_SCREEN_WIDTH := 1080p
-TARGET_SCREEN_HEIGHT := 1920
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys"
+
+# OrionOS stuff
+ORION_MAINTAINER := RomiYusnandar
+ORION_MAINTAINER_LINK := https://t.me/romiyusna
+ORION_GAPPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS := false
+
+# Common flags
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
