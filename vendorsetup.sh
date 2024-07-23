dc331f6906e0664e3401a5054aa3f4b7f8873d25
romiyusnandar <yusromi04@gmail.com>
2024-07-23 06:35:54 +0000

    sweet: Use ```vendorseutup``` for easier track device tree

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/romiyusnandar/proprietary_vendor_xiaomi_sweet.git -b testing vendor/xiaomi/sweet
+git clone https://github.com/omnitrix21/kernel_xiaomi_sweet kernel/xiaomi/sweet --depth=1
+git clone https://github.com/PixelOS-Devices/vendor_xiaomi_sweet-miuicamera vendor/xiaomi/sweet-miuicamera
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX  packages/apps/ViPER4AndroidFX
