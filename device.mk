01949d4a16745119184c4ed6fea0a50a557d0cd9
Rheygine Medel <rmedel@codeaurora.org>
2025-01-23 11:37:34 +0800

    sweet: Disable blur on app launch
    This change adds the system property that disables blur during app launch.
    
    Signed-off-by: donjohanliebert <donjohanliebert@gmail.com>
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- device.mk
+++ device.mk
@@ -33,7 +33,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1 \
-    persist.sys.sf.disable_blurs=1
+    persist.sys.sf.disable_blurs=1 \
+    ro.launcher.blur.appLaunch=0
 
 # Device Settings
 PRODUCT_PACKAGES += \
