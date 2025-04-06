670a8801638f7d3c2188d6413cfc441002daa8e0
romiyusnandar <yusromi04@gmail.com>
2025-04-06 06:25:14 +0000

    sweet: Bringup Ryu-UI Org
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- ryu_sweet.mk
+++ ryu_sweet.mk
@@ -12,16 +12,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Ryu-UI stuff.
+$(call inherit-product, vendor/ryu/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := ryu_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Ryu Flags
+RYU_MAINTAINER := RyuDev
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+USE_PIXEL_CHARGER := true
 
 BUILD_FINGERPRINT := Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
